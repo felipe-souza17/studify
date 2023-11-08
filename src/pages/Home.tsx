9de67d0d3faab1af9e969d9af44a9cf8e1dd2222
felipe-souza17 <felipesantos.rsouza@gmail.com>
2023-11-08 11:17:22 -0300

    Criação da navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
     return (
-        <h1 className="text-2xl">This is my homepage</h1>
+        <>
+            <Navbar/>
+        </>
     )
 }
