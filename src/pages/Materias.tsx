3e4df79d20f804aca9ac9c2352b7ae1157db6f44
felipe-souza17 <felipesantos.rsouza@gmail.com>
2023-11-28 19:27:19 -0300

    Implementação do video

--- src/pages/Materias.tsx
+++ src/pages/Materias.tsx
@@ -104,6 +104,16 @@ export default function Materias() {
             </p>
           </div>
         </div>
+        <iframe
+          className="mx-auto rounded-xl"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/lrRmCBgq6JE?si=rB7MeOhu1J3wfNEc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
       </section>
       <Footer />
     </>
